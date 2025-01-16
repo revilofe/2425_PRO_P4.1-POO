557c1b963e0152a18b572e31757133b7f627e650
Edu <revilofe@gmail.com>
2025-01-16 08:21:58 +0100

    Primer commit

--- src/main/kotlin/org/iesra/Main.kt
+++ src/main/kotlin/org/iesra/Main.kt
@@ -1,4 +1,4 @@
-package org.iesra.revilofe
+package org.iesra
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
